42f5dcf1fa5175cb685c0fbec49b3913781dac8b
vaz <vaz.matri@gmail.com>
2025-07-22 21:28:14 +0530

    create apple

--- game.js
+++ game.js
@@ -4,6 +4,17 @@ const appleShooter = (app) => {
     div.style.height = '500px'
     div.style.width = '500px'
     div.style.border = '1px solid blue'
+    div.style.position = 'relative'
+
+    const apple = document.createElement('div')
+    apple.style.height = '50px'
+    apple.style.width = '50px'
+    apple.style.background = 'green'
+    apple.style.left = '10px'
+    apple.style.top = '10px'
+    apple.style.position = 'absolute'
+
+    div.appendChild(apple)
 
     app.appendChild(div)
 }
