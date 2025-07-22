a693d3888150daaaf8792f928f5af85c8d3780ce
vaz <vaz.matri@gmail.com>
2025-07-22 22:53:31 +0530

    make apple move left

--- src/game-logic.js
+++ src/game-logic.js
@@ -2,7 +2,7 @@ import config from './config.js'
 import gameState from './game-state.js'
 import { updateApplePosition } from './render.js'
 
-export const moveRight = (cb) => {
+export const moveRight = () => {
     let left = gameState.position.left
 
     const rightMax = config.fence.width - config.apple.width
@@ -11,6 +11,19 @@ export const moveRight = (cb) => {
         // do nothing
     } else {
         left = left + 5
+        gameState.position.left = left
+
+        updateApplePosition(left, top)
+    }
+}
+
+export const moveLeft = () => {
+    let left = gameState.position.left
+
+    const leftMax = 0
+
+    if (left > leftMax) {
+        left = left - 5
         gameState.position.left = left
 
         updateApplePosition(left, top)
--- src/key-down.js
+++ src/key-down.js
@@ -1,9 +1,11 @@
-import { moveRight } from './game-logic.js'
+import { moveLeft, moveRight } from './game-logic.js'
 
 const keyPress = (fence) => {
     fence.addEventListener('keydown', (event) => {
         if (event.key === 'ArrowRight') {
             moveRight()
+        } else if (event.key === 'ArrowLeft') {
+            moveLeft()
         }
     })
 }
