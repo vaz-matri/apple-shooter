21615c334e9fdea169282dfbf85f1d2092c358ea
vaz <vaz.matri@gmail.com>
2025-07-22 21:44:10 +0530

    introduce game state

--- game.js
+++ game.js
@@ -1,18 +1,33 @@
+const gameState = {
+    fence: {
+        height: 500,
+        width: 500
+    },
+    apple: {
+        height: 50,
+        width: 50
+    },
+    position: {
+        left: 10,
+        top: 10
+    }
+}
+
 const appleShooter = (app) => {
     const div = document.createElement('div')
 
-    div.style.height = '500px'
-    div.style.width = '500px'
+    div.style.height = `${gameState.fence.height}px`
+    div.style.width = `${gameState.fence.width}px`
     div.style.border = '1px solid blue'
     div.style.position = 'relative'
     div.tabIndex = 0
 
     const apple = document.createElement('div')
-    apple.style.height = '50px'
-    apple.style.width = '50px'
+    apple.style.height = `${gameState.apple.height}px`
+    apple.style.width = `${gameState.apple.width}px`
     apple.style.background = 'green'
-    apple.style.left = '10px'
-    apple.style.top = '10px'
+    apple.style.left = `${gameState.position.left}px`
+    apple.style.top = `${gameState.position.top}px`
     apple.style.position = 'absolute'
 
     div.appendChild(apple)
